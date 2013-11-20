8822d8801b4500246485ef473244f9bbc81e7baa
Dan S Turner <turner@nowwithmoredan.com>
2013-11-19 16:38:13 -0800

    Add and pass test for II

--- roman.rb
+++ roman.rb
@@ -1,4 +1,6 @@
 class Fixnum
   def to_roman
+    arabic_array = self.to_s.chars.map(&:to_i)
+    arabic_array.map { |digit| "I" * arabic_array[-1]%5 if arabic_array[-1] >= 1 }.join
   end
 end
--- roman_test.rb
+++ roman_test.rb
@@ -6,5 +6,12 @@ class RomanTest < MiniTest::Unit::TestCase
     1.to_roman
   end
 
+  def test_for_1_returns_I
+    assert_equal "I", 1.to_roman
+  end
+
+  def test_for_2_returns_II
+    assert_equal "II", 2.to_roman
+  end
 
 end
